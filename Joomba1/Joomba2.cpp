57999032590d1ed23aeb05c5b06835d8fbd29c92
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2025-11-20 08:10:37 -0300

    avançando

--- Joomba1/Joomba2.cpp
+++ Joomba1/Joomba2.cpp
@@ -83,6 +83,48 @@ int32_t conta_instrucoes(instrucao_node* i) {
 	return 0;
 }
 
+
+/********
+COMBINAR O PRINT_PREDIO COM AS INSTRUCOES
+PARA DESENHAR O CAMINHO QUE ELE SUPOSTAMENTE TENTOU FAZER
+*********/
+static void print_caminho(instrucao_node* n, predio* p) {
+	if (!p) return;
+
+	predio* q = constroi_predio(p->ANDARES, p->JANELAS);
+	instrucao_node* node = n;
+	std::vector<instrucao_node*> instrucoes;
+	std::vector<int32_t> posicoes;
+	while (node) {
+		instrucoes.push_back(node);
+		node = node->parent;
+	}
+	memset(q->janelas_array, '.', q->ANDARES * q->JANELAS * 4);
+	posicoes.push_back(q->current_position);
+	while (!instrucoes.empty()) {
+		node = instrucoes.back();
+		fmt::println("DIRECAO: {}, DISTANCIA: {}", node->direcao, node->distancia);
+		switch (node->direcao) {
+		case 'E': move_esquerda(node->distancia, q); break;
+		case 'D': move_direita(node->distancia, q); break;
+		case 'C': move_para_cima(node->distancia, q); break;
+		case 'B': move_para_baixo(node->distancia, q); break;
+		default: break;
+		}
+		posicoes.push_back(q->current_position);
+		instrucoes.pop_back();
+	}
+	for (int32_t i : posicoes) q->janelas_array[i] = 'X';
+	fmt::println("TENTATIVA DE PRINTAR O CAMINHO QUE FOI FEITO!");
+	print_predio(q);
+	destroi_predio(q);
+}
+
+static char alterar_as_direcoes_para_evitar_lock() {
+	static int32_t index = 0;
+	static char direcoes[] = {'C', 'B', 'E', 'D'};
+	return direcoes[index++ % 4];
+}
 static int32_t conta_lista_consecutiva_de_direcao(const char direcao, instrucao_node* lista) {
 	int32_t counter = 0;
 	instrucao_node* node = lista;
@@ -100,11 +142,13 @@ static int32_t conta_lista_consecutiva_de_direcao(const char direcao, instrucao_
 bool checar_limpeza(instrucao_node* list_instrucoes, int32_t max_instrucoes_permitido, predio* p) {
 	// Verificar se o prédio está limpo
 	if (!p) return false;
+	//print_predio(p);
 	bool predio_esta_limpo = is_predio_limpo(p);
 
 	if (conta_instrucoes(list_instrucoes) > max_instrucoes_permitido) return false;
 	if (predio_esta_limpo) {
 		print_lista_instrucoes(list_instrucoes);
+		print_caminho(list_instrucoes, p);
 		return true;
 	}
 	// salva o estado atual da janela
@@ -112,34 +156,65 @@ bool checar_limpeza(instrucao_node* list_instrucoes, int32_t max_instrucoes_perm
 	int32_t current_position = p->current_position;
 	p->janelas_array[p->current_position] = '.'; // marquei a posição corrente como limpa
 
-	if (pode_ir_para_baixo(p)) {
-		move_para_baixo(1, p);
-		instrucao_node nova{'B', 1, list_instrucoes};
-		if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
-		p->current_position = current_position;
-	}
-	if (pode_ir_para_cima(p)) {
-		move_para_cima(1, p);
-		instrucao_node nova{ 'C', 1, list_instrucoes };
-		if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
-		p->current_position = current_position;
-	}
+
+	char minhas_direcoes[] = { 'C', 'B', 'E', 'D' };
+	char proxima_direcao = '\0';
+	do {
+		char sugestao = alterar_as_direcoes_para_evitar_lock();
+		proxima_direcao = '\0';
+		for (int32_t i = 0; i < 4; i++) {
+			if (sugestao == minhas_direcoes[i]) {
+				proxima_direcao = sugestao;
+				minhas_direcoes[i] = '\0';
+				break;
+			}
+		}
+		switch (proxima_direcao) {
+		case 'B':
+			if (pode_ir_para_baixo(p)) {
+				move_para_baixo(1, p);
+				instrucao_node nova{ 'B', 1, list_instrucoes };
+				if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
+				p->current_position = current_position;
+			}
+			break;
+		case 'C':
+			if (pode_ir_para_cima(p)) {
+				move_para_cima(1, p);
+				instrucao_node nova{ 'C', 1, list_instrucoes };
+				if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
+				p->current_position = current_position;
+			}
+			break;
+		case 'E':
+			if (conta_lista_consecutiva_de_direcao('E', list_instrucoes) < (4 * p->JANELAS / 2)) {
+				move_esquerda(1, p);
+				instrucao_node nova{ 'E', 1, list_instrucoes };
+				if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
+				p->current_position = current_position;
+			}
+			break;
+		case 'D':
+			if (conta_lista_consecutiva_de_direcao('D', list_instrucoes) < (4 * p->JANELAS / 2)) {
+				move_direita(1, p);
+				instrucao_node nova{ 'D', 1, list_instrucoes };
+				if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
+				p->current_position = current_position;
+			}
+			break;
+		default:
+			break;
+		}
+	} while (proxima_direcao);
+
+
+
+
 	// como as quatro faces do prédio formam um círculo
 	// é preciso andar no máximo metade das janelas para a esquerda ou direita
 	// para se chegar a qualquer lugar
 
-	if (conta_lista_consecutiva_de_direcao('E', list_instrucoes) < (4 * p->JANELAS / 2)) {
-		move_esquerda(1, p);
-		instrucao_node nova{ 'E', 1, list_instrucoes };
-		if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
-		p->current_position = current_position;
-	}
-	if (conta_lista_consecutiva_de_direcao('D', list_instrucoes) < (4 * p->JANELAS / 2)) {
-		move_direita(1, p);
-		instrucao_node nova{ 'D', 1, list_instrucoes };
-		if (checar_limpeza(&nova, max_instrucoes_permitido, p)) return true;
-		p->current_position = current_position;
-	}
+
 
 	// ele já tentou todas as direções possíveis. Então restaurar o valor da janela corrente
 	p->janelas_array[current_position] = current_janela_state;
