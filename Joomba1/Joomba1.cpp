be5bc1d49548f57be1ed7c1cdb6f863f54960faf
Gabriel Prado de Moraes <gpradodemoraes@outlook.com>
2025-11-25 14:35:21 -0300

    small changes

--- Joomba1/Joomba1.cpp
+++ Joomba1/Joomba1.cpp
@@ -1,23 +1,22 @@
 ﻿#include "Joomba1.h"
 #include <string>
 #include <sstream>
-#include <regex>
 #include <fstream>
 #include "predio.h"
 
-bool parse_entrada_joomba1(char* filepath, std::vector<instrucao>* instrucoes, int32_t *ANDARES_, int32_t *JANELAS_) {
+bool parse_entrada_joomba1(char* filepath, std::vector<instrucao>* instrucoes, int32_t* ANDARES_, int32_t* JANELAS_) {
 	std::ifstream file(filepath);
 	if (file.good() && file.is_open()) {
 		int32_t JANELAS = 0;
 		int32_t ANDARES = 0;
 		std::string line;
 		while (getline(file, line)) {
-			//fmt::println("|=>{}", line);
+			// fmt::println("|=>{}", line);
 			std::stringstream ss(line);
 			std::string word;
 			instrucao i{};
 			while (ss >> word) {
-				if(word.at(0) == 'F') break;
+				if (word.at(0) == 'F') break;
 				if (ANDARES == 0) {
 					ANDARES = stoi(word);
 					continue;
@@ -44,35 +43,24 @@ bool parse_entrada_joomba1(char* filepath, std::vector<instrucao>* instrucoes, i
 	}
 	return false;
 }
-bool processa_entrada_joomba_1(std::vector<instrucao>* instrucoes,
-	const int32_t ANDARES,
-	const int32_t JANELAS) {
 
+bool processa_entrada_joomba_1(std::vector<instrucao>* instrucoes, const int32_t ANDARES, const int32_t JANELAS) {
 	predio* p = constroi_predio(ANDARES, JANELAS);
 
-	while (!instrucoes->empty())  {
+	int32_t coco = p->janelas_array[12];
+	while (!instrucoes->empty()) {
 		instrucao i = instrucoes->back();
 		switch (i.direcao) {
-		case 'D':
-			move_direita(i.distancia, p);
-			break;
-		case 'E':
-			move_esquerda(i.distancia, p);
-			break;
-		case 'C':
-			move_para_cima(i.distancia, p);
-			break;
-		case 'B':
-			move_para_baixo(i.distancia, p);
-			break;
-		default:
-			break;
+			case 'D': move_direita(i.distancia, p); break;
+			case 'E': move_esquerda(i.distancia, p); break;
+			case 'C': move_para_cima(i.distancia, p); break;
+			case 'B': move_para_baixo(i.distancia, p); break;
+			default: break;
 		}
 		instrucoes->pop_back();
 	}
 	print_predio(p);
-	//fmt::println("\nARRAY {}", fmt::join(janelas_array, janelas_array + array_size, ""));
+	// fmt::println("\nARRAY {}", fmt::join(janelas_array, janelas_array + array_size, ""));
 	destroi_predio(p);
 	return true;
-
 }
